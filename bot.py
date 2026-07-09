30f7a6e968410758b9eeb13732bb79bcbec272eb
rijwanansari2 <ra3015597@gmail.com>
2026-07-09 11:02:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is working ✅")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
